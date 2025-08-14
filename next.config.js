fa80a2b86870f1b25c6d85b7ff2ba7e54dd771ab
jcoelhocontatu-max <jcoelho.contatu@gmail.com>
2025-08-13 23:39:55 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'via.placeholder.com'
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ]
+  },
+  compress: true,
+  poweredByHeader: false,
+}
+
+module.exports = nextConfig
